5189b0af96e79bb46159753e7ecc06a7cc98744d
Jasmina Zejnilovic <jasmina.zejnilovic995@gmail.com>
2021-03-25 19:07:39 +0100

    Mapping and Filtering

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -2,9 +2,9 @@
 let one = new Set([1, 2, 3, 4, 5]);
 let two = new Set([6, 4, 3, 2]);
 
-let unionSet = new Set([...one].concat(...two));
-let intersectionSet = new Set([...one].filter((x) => [...two].includes(x)));
-let differenceSet = new Set([...one].filter((x) => ![...two].includes(x)));
+const unionSet = new Set([...one].concat(...two));
+const intersectionSet = new Set([...one].filter((x) => [...two].includes(x)));
+const differenceSet = new Set([...one].filter((x) => ![...two].includes(x)));
 
 console.log(one);
 console.log(two);
